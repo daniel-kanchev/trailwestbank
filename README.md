bc19b2c1d3b44127bc87594d330f7d3559a39a66
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-30 10:57:00 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://trailwest.bank/news/
+
+    Spider name: trailwestbank
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ trailwestbank/spiders/trailwestbank.py
@@ -0,0 +1,44 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst
+from datetime import datetime
+from trailwestbank.items import Article
+
+
+class trailwestbankSpider(scrapy.Spider):
+    name = 'trailwestbank'
+    start_urls = ['https://trailwest.bank/news/']
+
+    def parse(self, response):
+        links = response.xpath('//h2/a/@href').getall()
+        yield from response.follow_all(links, self.parse_article)
+
+        next_page = response.xpath('//a[@class="next page-numbers"]/@href').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
+
+    def parse_article(self, response):
+        if 'pdf' in response.url:
+            return
+
+        item = ItemLoader(Article())
+        item.default_output_processor = TakeFirst()
+
+        title = response.xpath('//h1/text()').get()
+        if title:
+            title = title.strip()
+
+        date = response.xpath('//time/text()').get()
+        if date:
+            date = date.strip()
+
+        content = response.xpath('//section[@itemprop="articleBody"]//text()').getall()
+        content = [text for text in content if text.strip()]
+        content = "\n".join(content).strip()
+
+        item.add_value('title', title)
+        item.add_value('date', date)
+        item.add_value('link', response.url)
+        item.add_value('content', content)
+
+        return item.load_item()
